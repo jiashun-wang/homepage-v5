3766390751625e14f4f662ec732c08af048b9af9
Jiashun Wang <wjs@ieee.org>
2026-08-26 02:44:32 +0800

    add reposal

---
+++ app/pages/template-dco.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+useHead({ title: 'Template' })
+definePageMeta({ headerText: '模板' })
+
+const projects = [
+	{
+		title: 'ucas-research-proposal',
+		github: 'jiashun-wang/ucas-research-proposal',
+		description: '非官方的中国科学院博士生研究计划书',
+	},
+
+]
+</script>
+
+<template>
+<section>
+	<ZProject v-for="project in projects" :key="project.title" v-bind="project" />
+</section>
+</template>
+
+<style scoped>
+section {
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
+	gap: 1rem;
+}
+</style>
---
+++ app/pages/template-slide.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+useHead({ title: 'Template' })
+definePageMeta({ headerText: '模板' })
+
+const projects = [
+	{
+		title: 'Rss-Translation',
+		github: 'jiashun-wang/Rss-Translation',
+		description: '学术期刊RSS翻译和再分发',
+	},
+]
+</script>
+
+<template>
+<section>
+	<ZProject v-for="project in projects" :key="project.title" v-bind="project" />
+</section>
+</template>
+
+<style scoped>
+section {
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
+	gap: 1rem;
+}
+</style>
